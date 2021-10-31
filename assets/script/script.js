0db7853556e17fe3483c8032b0b4b5110c957dbd
“Mat-Techno” <matteo.courquin@gmail.com>
2021-11-01 00:42:49 +0100

    add animation & section a propos

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,28 @@
 window.onload = () => {
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
         let hauteur = document.documentElement.scrollHeight - window.innerHeight;
         let position = window.scrollY;
         let largeur = document.documentElement.clientWidth;
 
         let barre = (position / hauteur) * largeur;
 
-        document.getElementById("barre").style.width = barre + "px";
+        document.getElementById('barre').style.width = barre + "px";
     });
 };
+
+const logoDev = document.querySelectorAll('.skills_items')
+let detailSkills = document.querySelectorAll('.details_skills')
+
+function hello() {
+    detailSkills.forEach(itemSkills => {
+        itemSkills.classList.add('active')
+    });
+}
+
+logoDev.forEach(item => {
+    item.addEventListener('mouseenter', function () {
+        detailSkills.forEach(itemSkills => {
+            itemSkills.classList.add('active')
+        });
+    })
+});
