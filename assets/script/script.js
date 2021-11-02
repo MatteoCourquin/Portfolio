09902ea8baeb40cd9dbc31140ecd87ba3f3f15ac
“Mat-Techno” <matteo.courquin@gmail.com>
2021-11-02 22:33:27 +0100

    add animation scroll

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,10 +13,59 @@ window.onload = () => {
     });
 };
 
+// **** COLOR ****
+// ===============
+let root = document.documentElement;
+let btnColorViolet= document.querySelector('.violet');
+let btnColorPink = document.querySelector('.pink');
+let btnColorBlue = document.querySelector('.blue');
+
+
+function changeColor(color) {
+    // console.log('change color !')
+    root.style.setProperty('--mainColor', color)
+   
+};
+
+btnColorViolet.addEventListener('click', function(){
+    changeColor("#9984D4")
+})
+btnColorPink.addEventListener('click', function(){
+    changeColor("#FF4D80")
+});
+btnColorBlue.addEventListener('click', function() {
+    changeColor("#0075F2")
+});
+
+
+
+// **** SHOW SCROLL ****
+// =====================
+const ratio = .5;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const show_scroll = function (entries, observer) {
+    entries.forEach(function(entry){
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('show_scroll')
+            observer.unobserve(entry.target)
+                console.log('visible');
+        }
+    })
+}
+
+const observer = new IntersectionObserver(show_scroll, options)
+document.querySelectorAll('[class*="anim_scroll"]').forEach(function(r){
+    observer.observe(r);
+})
 
 
 // **** SHOW SKILLS ****
-// ===================
+// =====================
 const logoDev = document.querySelectorAll('.skills_items')
 let detailSkills = document.querySelectorAll('.details_skills')
 
@@ -43,29 +92,3 @@ var copyright = document.querySelector('.copyright')
 // year.toString();
 
 copyright.textContent = (year);
-
-
-
-// **** COLOR ****
-// ===============
-let root = document.documentElement;
-let btnColorViolet= document.querySelector('.violet');
-let btnColorPink = document.querySelector('.pink');
-let btnColorBlue = document.querySelector('.blue');
-
-
-function changeColor(color) {
-    // console.log('change color !')
-    root.style.setProperty('--mainColor', color)
-   
-};
-
-btnColorViolet.addEventListener('click', function(){
-    changeColor("#9984D4")
-})
-btnColorPink.addEventListener('click', function(){
-    changeColor("#FF4D80")
-});
-btnColorBlue.addEventListener('click', function() {
-    changeColor("#0075F2")
-});
