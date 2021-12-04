49b5faacac1ae70f217869d572b905f6c7da0f77
« Matteo <matteo.courquin@gmail.com>
2021-12-04 10:03:40 +0100

    :lipstick: add cursor

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,6 +7,24 @@ window.addEventListener('load', () => {
 })
 
 
+// **** CURSOR ****
+// ================
+
+const cursor = document.querySelector('.new-cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
+
+
 // **** PROGRESSION LINE TOP ****
 // ==============================
 window.onload = () => {
